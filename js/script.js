02a6c91d987b5cfaee2bf59d7deb1700f44849ff
TTYnWEB <dmhepburn@gmail.com>
2025-05-09 17:08:14 -0500

    DATA - PERMISSIONS obj for maintenance/typo-avoidance, urlRegex similar to the pattern used for html url input validity checker, now that there are multiple modals adding some helper funcs to apply to only open modals, setClipPref func to deduplicate functionality, showShortURL func clean up addURL, autoPaste now has isParseable which uses urlRegex, debounce timeout added to urlInput, added clipboard-read helper modal to clip-read toggle, modal funcs changed to apply to only open modals (closeBtn applying to all), some template code for SW

--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-// ELEMs
+//[ ELEM ]/////////////////////////////////////////////////////////////////////
 const html          = document.documentElement;
 const form          = document.getElementById('form');
 const clipR         = document.getElementById('clip-r');
 const clipW         = document.getElementById('clip-w');
 const copyBtn       = document.getElementById('copy');
+const installBtn    = document.getElementById('install-btn');
 const urlInput      = document.getElementById('url');
 const themeToggle   = document.getElementById('theme-toggle');
 const invalidHelp   = document.getElementById('invalid-helper');
 const submitButton  = document.getElementById('submit-btn');
 const inputShortURL = document.getElementById('short-url');
 const modalShortURL = document.getElementById('modal-short-url');
-const closeBtn      = document.querySelector('[aria-label="Close"]');
+const modalClipR    = document.getElementById('modal-clip-r');
+const modals        = document.querySelectorAll('[id^="modal-"]');
+const closeBtn      = document.querySelectorAll('[aria-label="Close"]');
+
+//[ DATA ]/////////////////////////////////////////////////////////////////////
+const PERMISSIONS = {
+  CLIPBOARD_READ: 'clipboard-read',
+  CLIPBOARD_WRITE: 'clipboard-write',
+};
+
+const urlRegex = /^https?:\/\/[^\s/$.?#].[^\s]*$/i;
 
 //[ FUNC ]/////////////////////////////////////////////////////////////////////
 const setTheme = mode => {
@@ -32,13 +43,25 @@ const setInitialTheme = () => {
 };
 
 const selectText = e => e.target.select();
+//const isModalOpen = () => modalShortURL.open;
+const isModalOpen = () => [...modals].some(({ open }) => open);
+const queryPermissions = async (name) => navigator.permissions.query({ name });
 
 const toggleValidity = bool => {
   submitButton.disabled = bool;
   urlInput.setAttribute('aria-invalid', bool);
 };
 
-const queryPermissions = async (name) => navigator.permissions.query({ name });
+const setClipPref = (elem, key, bool) => {
+  elem.checked = bool;
+  const boolStr = (bool ? 'true' : 'false');
+  localStorage.setItem(key, boolStr);
+};
+
+const setClipRstate = bool => {
+  clipR.checked = bool;
+  localStorage.setItem('clip-r', `${bool}`);
+};
 
 const toggleSetting = async (elem, itemName, permissionName) => {
   const val = localStorage.getItem(itemName);
@@ -54,8 +77,21 @@ const toggleSetting = async (elem, itemName, permissionName) => {
 };
 
 const initSettings = () => {
-  toggleSetting(clipR, 'clip-r', 'clipboard-read');
-  toggleSetting(clipW, 'clip-w', 'clipboard-write');
+  toggleSetting(clipR, 'clip-r', PERMISSIONS.CLIPBOARD_READ);
+  toggleSetting(clipW, 'clip-w', PERMISSIONS.CLIPBOARD_WRITE);
+};
+
+const showShortURL = key => {
+  invalidHelp.setAttribute('hidden', true);
+
+  const shortURL = `https://2to.co/${key}`;
+  inputShortURL.value = shortURL;
+  modalShortURL.showModal();
+  inputShortURL.select();
+
+  const isClipW = (localStorage.getItem('clip-w') === 'true');
+  if (isClipW)
+    navigator.clipboard.writeText(inputShortURL.value);
 };
 
 const addURL = async () => {
@@ -67,16 +103,7 @@ const addURL = async () => {
   };
   const res = await fetch('/api/add', payload)
     .then(res => res.json())
-    .then(data => {
-      invalidHelp.setAttribute('hidden', true);
-      const shortURL = `https://2to.co/${data.key}`;
-      inputShortURL.value = shortURL;
-      modalShortURL.showModal();
-      const isClipW = (localStorage.getItem('clip-w') === 'true');
-      if (isClipW)
-        navigator.clipboard.writeText(inputShortURL.value);
-      inputShortURL.select();
-    })
+    .then(({ key }) => showShortURL(key))
     .catch(err => {
       invalidHelp.removeAttribute('hidden');
       submitButton.disabled = true;
@@ -89,93 +116,110 @@ const toggleSubmitBtn = () => {
     urlInput.removeAttribute('aria-invalid');
     return null;
   }
-
-  if (!invalidHelp.hasAttribute('hidden'))
-    invalidHelp.setAttribute('hidden', true);
-
-  if (urlInput.validity.valid)
-    toggleValidity(false);
-  else
-    toggleValidity(true);
+  invalidHelp.setAttribute('hidden', true);
+  toggleValidity(!urlInput.validity.valid);
 };
 
 const autoPaste = async () => {
-  const { state } = await navigator.permissions.query({ name: 'clipboard-read' });
-  if (state !== 'granted')
+  const { state } = await queryPermissions(PERMISSIONS.CLIPBOARD_READ);
+  if ((state !== 'granted') || (!document.hasFocus()))
     return null;
 
   const isClipR = (localStorage.getItem('clip-r') === 'true');
   if (isClipR) {
     const clipContent = await navigator.clipboard.readText();
+    const isParseable = urlRegex.test(clipContent);
+    if (!isParseable)
+      return null;
     urlInput.value = clipContent;
     toggleSubmitBtn();
     urlInput.focus();
   }
 };
+//[ EventListeners ]////////////////////////////////////////////////////////////
+// EL - theme
+themeToggle.addEventListener('change', () => (
+  setTheme(themeToggle.checked ? 'light' : 'dark')
+));
 
-//[ EventListener - theme ]/////////////////////////////////////////////////////
-themeToggle.addEventListener('change', () => {
-  if (themeToggle.checked)
-    setTheme('light');
-  else
-    setTheme('dark');
-});
-
-//[ EventListener - input ]////////////////////////////////////////////////////
+// EL - input
 // Event listener to enable/disable submit button based on input value
 // validatorjs npm pkg - isURL() func
-urlInput.addEventListener('input', toggleSubmitBtn);
+let debounceTimeout;
+urlInput.addEventListener('input', () => {  // NOTE: should be change (review all change vs input)?
+  clearTimeout(debounceTimeout);
+  debounceTimeout = setTimeout(toggleSubmitBtn, 100);
+});
 
-//[ EventListener - form submit ]///////////////////////////////////////////////
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
+// EL - form submit
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
   addURL();
 });
 
-//[ EventListener - clip-r ]////////////////////////////////////////////////////
+// EL - clip-r
 // NOTE: doesn't work on ffox
 clipR.addEventListener('change', async () => {
-  clipRtoggle = bool => {
-    clipR.checked = bool;
-    localStorage.setItem('clip-r', `${bool}`);
-  };
+  const onIntention = clipR.checked;
+  const { state: clipRpermState } = await queryPermissions(PERMISSIONS.CLIPBOARD_READ);
+  const [
+    clipRpermGranted,
+    clipRpermDenied,
+  ] = [
+    (clipRpermState === 'granted'),
+    (clipRpermState === 'denied'),
+  ];
+  if ((onIntention && clipRpermDenied) || (!onIntention && clipRpermGranted))
+    modalClipR.showModal();
+    
   await navigator.clipboard.readText()
-    .then(() => clipRtoggle(true))
-    .catch(() => clipRtoggle(false));
+    .then(() => setClipPref(clipR, 'clip-r', true))
+    .catch(() => setClipPref(clipR, 'clip-r', false))
+
 });
 
-//[ EventListener - clip-w ]////////////////////////////////////////////////////
-clipW.addEventListener('change', async () => (
-  localStorage.setItem('clip-w', (clipW.checked) ? 'true' : 'false')
+// EL - clip-w
+clipW.addEventListener('change', () => (
+  setClipPref(clipW, 'clip-w', clipW.checked)
 ));
 
-//[ EventListeners - shortURL ]/////////////////////////////////////////////////
+// EL - shortURL
 inputShortURL.addEventListener('click', selectText);
 inputShortURL.addEventListener('touchstart', selectText);
 
-//[ EventListeners - modal ]////////////////////////////////////////////////////
-// Close with a click outside
-document.addEventListener('click', (e) => {
-  const modalIsVisible = modalShortURL.open;
-  if (!modalIsVisible)
+// EL - modal
+//    - modal - Close with a click outside
+modals.forEach(modal => {
+  modal.addEventListener('click', (event) => {
+    const article = modal.querySelector('article');
+    if (article && !article.contains(event.target)) {
+      modal.close();
+    }
+  });
+});
+
+//    - modal - Close with Close btn
+closeBtn.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const openModal = [...modals].find(modal => modal.open);
+    openModal.close();
+  //modals.forEach(modal => modal.close())
+  //modalShortURL.close()
+  });
+});
+
+//    - modal - Close with ESC key
+document.addEventListener('keydown', (event) => {
+  if (!isModalOpen())
     return;
-  const modalContent = modalShortURL.querySelector('article');
-  const isClickInside = modalContent.contains(e.target);
-  if (!isClickInside)
-    modalShortURL.close();
+  const openModal = [...modals].find(modal => modal.open);
+  if (event.key === "Escape")
+    openModal.close();
+    //modals.forEach(modal => modal.close());
+    //modalShortURL.close();
 });
 
-// Close with Esc key)
-document.addEventListener('keydown', (e) => {
-  const modalIsVisible = () => modalShortURL.open;
-  if (modalIsVisible && (e.key === "Escape"))
-    modalShortURL.close();
-});
-
-// Close with Close btn
-closeBtn.addEventListener('click', () => modalShortURL.close());
-
-// copy Short URL to clipboard
+//    - modal - copyBtn - copy Short URL to clipboard
 copyBtn.addEventListener('click', () => (
   navigator.clipboard.writeText(inputShortURL.value)
 ));
@@ -197,7 +241,34 @@ const uaDetection = () => {
   else if (/Edge/.test(userAgent))
     console.log('Edge detected');
 };
-//themeToggle.addEventListener('change', () => setTheme(
-//  (themeToggle.checked) ? 'light' : 'dark'
-//));
+
+in toggleSubmitBtn:
+  //if (!invalidHelp.hasAttribute('hidden'))
+  //if (urlInput.validity.valid)
+  //  toggleValidity(false);
+  //else
+  //  toggleValidity(true);
+
+//urlInput.addEventListener('input', toggleSubmitBtn);
+
+// SW
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (event) => {
+  event.preventDefault();
+  deferredPrompt = event;
+  console.log({ deferredPrompt });
+  installBtn.removeAttribute('hidden');
+});
+installBtn.addEventListener('click', async () => {
+  if (deferredPrompt) {
+    console.log('before prompt');
+    deferredPrompt.prompt();
+    console.log('after prompt');
+    const { outcome } = await deferredPrompt.userChoice;
+    console.log(`User response to the install prompt: ${outcome}`);
+    deferredPrompt = null;
+    installBtn.setAttribute('hidden', null);
+  }
+});
+
 */
